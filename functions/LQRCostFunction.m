17eab7048a49a66e08010d6c3df07be8b4deb169
jengstud <jens.engel@stud.tu-darmstadt.de>
2021-06-29 16:54:15 +0200

    improved LQR cost function

--- functions/LQRCostFunction.m
+++ functions/LQRCostFunction.m
@@ -9,6 +9,7 @@ classdef LQRCostFunction < CostFunction
         Q_rep
         R_rep
         ref
+        N_pred
     end
     
     methods
@@ -31,6 +32,7 @@ classdef LQRCostFunction < CostFunction
                          zeros(length(Q), length(Q_rep)) Q_f];
             obj.R_rep = kron(eye(N_pred), R);
             obj.ref = ref;
+            obj.N_pred = N_pred;
         end
         
         function expr = buildExpression(obj, x, u, d, params, Ns, slacks, T_s)
@@ -43,18 +45,46 @@ classdef LQRCostFunction < CostFunction
         function [exprSingle] = buildExpressionSingleScen(obj, x, u, d, params, slacks, T_s, s)
             x_flat = x(:);
             u_flat = u(:);
-            ref = obj.ref;
-            if ischar(obj.ref)
-                ref = params.(ref);
-            end   
-            ref = ref(:);
+            x_ref = obj.ref;
             
-            exprSingle = (x_flat-ref)' * obj.Q_rep * (x_flat-ref) + u_flat' * obj.R_rep * u_flat;
+            % if reference signal is a char/string, then it's a parameter
+            if ischar(obj.ref) || isstring(obj.ref)
+                % check if parameter is scenario dependent
+                if iscell(params.(x_ref))
+                    x_ref = params.(x_ref){s};
+                else
+                    x_ref = params.(x_ref);
+                end
+                
+                % if reference signal has only one column, it's a reference point
+                if size(x_ref, 2) == 1
+                    x_ref = repmat(x_ref, 1, obj.N_pred+1);
+                end
+            end   
+            x_ref = x_ref(:);
+            
+            exprSingle = (x_flat-x_ref)' * obj.Q_rep * (x_flat-x_ref) + u_flat' * obj.R_rep * u_flat;
         end
         
         function [horizon] = evaluateHorizon(obj, x, u, d, params, slacks, T_s)
+            x_ref = obj.ref;
+            % if reference signal is a char/string, then it's a parameter
+            if ischar(obj.ref) || isstring(obj.ref)
+                % check if parameter is scenario dependent
+                if iscell(params.(x_ref))
+                    x_ref = params.(x_ref){1};
+                else
+                    x_ref = params.(x_ref);
+                end
+                
+                % if reference signal has only one column, it's a reference point
+                if size(x_ref, 2) == 1
+                    x_ref = repmat(x_ref, 1, obj.N_pred+1);
+                end
+            end   
+            
             for i=1:size(u, 2)
-                horizon(i) = x{1}(:, i)' * obj.Q * x{1}(:, i) + u(:, i)' * obj.R * u(:, i);
+                horizon(i) = (x{1}(:, i) - x_ref(:, i))' * obj.Q * (x{1}(:, i) - x_ref(:, i)) + u(:, i)' * obj.R * u(:, i);
             end
         end
     end
